7591c3ff0d0299bd3af5b86a7f9982e65702241e
seinpyo <ticket9424@gmail.com>
2022-01-24 16:14:47 +0900

    2022-01-24

--- WEB12_Shoppingmall/WebContent/sql.sql
+++ WEB12_Shoppingmall/WebContent/sql.sql
@@ -127,20 +127,21 @@ insert into product(pseq, name, kind, price1, price2, price3, content, image,bes
 (product_seq.nextval, '슬리퍼', '4', 5000,5500,500,'편안한 슬리퍼 입니다', 'w-25.jpg', 'y');
 insert into product(pseq, name, kind, price1, price2, price3, content, image,bestyn) values
 (product_seq.nextval, '회색힐', '1', 10000,12000,2000,'여성 전용 힐 입니다', 'w9.jpg', 'n');
-insert into product(pseq, name, kind, price1, price2, price3, content, image) values
-(product_seq.nextval, '여성부츠', '2', 12000,18000,6000,'여성용 부츠입니다', 'w4.jpg', 'n');
 
+insert into product(pseq, name, kind, price1, price2, price3, content, image, bestyn) values
+(product_seq.nextval, '여성부츠', '2', 12000,18000,6000,'여성용 부츠입니다', 'w4.jpg', 'n');
 insert into product(pseq, name, kind, price1, price2, price3, content, image,bestyn) values
 (product_seq.nextval, '핑크샌들', '3', 5000,5500,500,'사계절용 샌들입니다', 'w-10.jpg', 'y');
 
 insert into product(pseq, name, kind, price1, price2, price3, content, image,bestyn) values
 (product_seq.nextval, '슬리퍼', '3', 5000,5500,500,'편안한 슬리퍼 입니다', 'w11.jpg', 'y');
-insert into product(pseq, name, kind, price1, price2, price3, content, image) values
-(product_seq.nextval, '스니커즈', '4', 15000,20000,5000,'활동성이 좋은 스니커즈 입니다', 'w1.jpg');
 insert into product(pseq, name, kind, price1, price2, price3, content, image,bestyn) values
-(product_seq.nextval, '샌들', '3', 5000,5500,5000,'사계절용 샌들입니다 입니다', 'w-09.jpg', 'n');
+(product_seq.nextval, '스니커즈', '4', 15000,20000,5000,'활동성이 좋은 스니커즈 입니다', 'w1.jpg', 'n');
+insert into product(pseq, name, kind, price1, price2, price3, content, image,bestyn) values
+(product_seq.nextval, '샌들', '3', 5000,5500,5000,'사계절용 샌들입니다 입니다', 'w-24.jpg', 'n');
 insert into product(pseq, name, kind, price1, price2, price3, content, image,bestyn) values
 (product_seq.nextval, '스니커즈', '5', 15000,20000,5000,'활동성이 좋은 스니커즈 입니다', 'w-05.jpg', 'n');
+
 update product set bestyn = 'y' where pseq=11;
 select * from product
 
