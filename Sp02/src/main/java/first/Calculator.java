8bde21eebf3b2f8f3cdaa0dc66d085487904dbb8
seinpyo <ticket9424@gmail.com>
2022-02-03 15:53:43 +0900

    2022-02-03

---
+++ Sp02/src/main/java/first/Calculator.java
@@ -0,0 +1,7 @@
+package first;
+
+public interface Calculator {
+
+	public int cal (int firstNum, int secondNum);
+	
+}
---
+++ Sp02/src/main/java/first/Multi.java
@@ -0,0 +1,8 @@
+package first;
+
+public class Multi implements Calculator{
+	public int cal(int firstNum, int secondNum) {
+		int result = firstNum * secondNum;
+		return result;
+	}
+}
