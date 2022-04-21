b864c59ed651afcd23ad26e1cc5451bb80f53cbf
seinpyo <91402281+seinpyo@users.noreply.github.com>
2022-04-21 15:42:34 +0900

    2022-04-21

--- Node.js/03_HttpServer/05_Server05.js
+++ Node.js/03_HttpServer/05_Server05.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const fs = require('fs').promises
 
-const users = {} 
+let users = {} 
 //서버가 종료될 때까지 값이 유지되는 변수
 
 http.createServer( async (req, res)=>{
@@ -11,22 +11,65 @@ http.createServer( async (req, res)=>{
                 //첫번째 페이지로 갈 때
                 const data = await fs.readFile('./05_Front.html')
                 res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
-                res.end(data)
                 return res.end(data)
                 //파일 내용 전송 후 async(req, res)=>{} 함수가 
                 //종료되도록 return
                 //return이 없으면 try문 아래에 400 NOTFOUND도 같이 실행되게 됨
+            } else if(req.url === '/about'){
+                const data = await fs.readFile('./05_about.html')
+                res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'})
+                return res.end(data)
             } else if(req.url === '/users'){
-
+                return res.end(JSON.stringify(users))
+                //user 객체 안의 내용을 json 형식으로 변경하여 전송
             }
         } else if (req.method == 'POST'){
             if(req.url === '/user') {
-
+                //req에 전송된 자료를 stream 형식으로 받아 body변수에 대입
+                let body = ''
+                req.on('data', (data)=>{
+                    body += data
+                })
+                //req.on() : request의 동작을 첫 번째 인수로 전달된 키워드로 구분, 익명함수를 실행한다. 
+                //전달된 자료를 모두 객체 형식으로 받아서 처리한다. 
+                return req.on('end', ()=> {
+                    const {name} = JSON.parse(body)
+                    //전달된 데이터를 name 변수에 저장
+                    const id = Date.now() //id 변수에 날짜를 추출
+                    users[id] = name;
+                    res.writeHead(201, {'Content-Type' : 'text/plain; charset=utf-8'})
+                    res.end('ok') //함수에서 빠져 나감
+                })
             } 
         } else if (req.method == 'PUT') {
+            if(req.url.startsWith('/user/')){
+                // axios.put('/user/' + key, {name}) 이 전달되기 때문에 
+                // '/user/'로 시작하는 주소를 찾도록 함
+                
+                const key = req.url.split('/')[2] 
+                //  req.url.split('/')[0]: , [1]: user, [2]: key
+                console.log(key)
+                // 1650515427987 (POST ('/user')에서 설정한 Date.now()의 값)
 
+                let body = ''
+                req.on('data', (data)=> {
+                    body+= data
+                })
+
+                return req.on('end', ()=>{
+                    users[key] = JSON.parse(body).name
+                    res.writeHead(201, {'Content-Type' : 'text/plain; charset=utf-8'})
+                    res.end('ok')
+                })
+            
+            }
         } else if (req.method == 'DELETE'){
-
+            if(req.url.startsWith('/user/')){
+                const key = req.url.split('/')[2]
+                delete users[key]
+                res.writeHead(201, {'Content-Type' : 'text/plain; charset=utf-8'})
+                return res.end('ok')
+            }
         }
 
         //위 if-else 문 어디에도 걸리지 않을 경우 
---
+++ Node.js/03_HttpServer/06_SessionServer.js
@@ -0,0 +1,65 @@
+const http = require('http')
+const fs = require('fs').promises
+const url = require('url')
+const qs = require('querystring')
+
+const parseCookies = (cookie='') =>
+    cookie 
+        .split(';')
+        .map(v=>v.split('='))
+        .reduce((acc, [k,v]) => {
+            acc[k.trim()] = decodeURIComponent(v)
+            return acc
+        }, {})
+
+const session = {}
+
+http.createServer(async(req, res) => {
+    const cookies = parseCookies(req.headers.cookie) 
+    
+    if(req.url.startsWith('/login')) {
+        const {query} = url.parse(req.url)
+        const {name} = qs.parse(query)
+        
+        //세션, 쿠키 수명 계산
+        const expires = new Date()
+        expires.setMinutes(expires.getMinutes() + 1)
+
+        //세션 객체에 저장하기 위한 고유 키값
+        const uniqueInt = Date.now() 
+
+        //Cookies -> name = ${uniqueInt}
+        //Session -> ${uniqueInt} : '홍길동'
+        //의 형태로 저장됨
+        //세션의 값들은 서버에서 관리 : 세션에서 해당값의 유무, 쿠키 값의 유무를 검사해 조회한다
+
+        session[uniqueInt] = {
+            name, 
+            expires,
+        }
+
+        res.writeHead(302, {
+            Location: '/',
+            'Set-Cookie' : `session=${uniqueInt}; Expires=${expires.toGMTString()}; HttpOnly; Path=/`
+        })  
+        //쿠키에는 고유 키 값(uniqueInt)을 value로써 session key에 저장 (실제 값은 없음)
+
+    } else if(cookies.session && session[cookies.session].expires > new Date() ) { 
+        //쿠키에 session이라는 key가 존재 && session의 유효시간이 지나지 않있을 때
+
+        console.log(cookies.session)
+        res.writeHead(200, {'Content-Type' : 'text/plain; charset=utf-8'})
+        res.end(`${session[cookies.session].name}님 안녕하세요`)
+
+    } else {
+        try {
+            const data = await fs.readFile('./06_Cookie_page.html')
+            res.end(data)
+        } catch(err) {
+            res.writeHead(500, {'Content-Type' : 'text/plain; charset=utf-8'})
+            res.end(err.message)
+        }
+    }
+}).listen(8090, () => {
+    console.log('Server is ready in port 8090')
+})
