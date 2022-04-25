4624791dab3936340697e03450c8c5ca02fd9750
seinpyo <91402281+seinpyo@users.noreply.github.com>
2022-04-25 15:45:27 +0900

    2022-04-25

---
+++ Node.js/04_ExpressServer/npm12/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const nunjucks = require('nunjucks')
+
+const app = express()
+app.set('port', process.env.PORT || 3000)
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.set('view engine', 'html') 
+nunjucks.configure('views',{
+    express: app,
+    watch: true
+})
+
+
+app.listen(app.get('port'), () => {
+    console.log('the Server is ready in port', app.get('port'))
+})
